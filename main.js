e8c8054c215b97bf2302106a76bea689b0d13bc3
Alexandre Martinez Melo <alexandre.martinezmelo@emeal.nttdata.com>
2023-12-06 15:06:27 +0000

    mixed messages algorithm done

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ const ramdomNumber = num => {
 
 /* we put the ramdom message in an array of arrays;*/
 
- let pronouns = ["I", "You", "He", "She", "They", "Them", "We"];
- let verbs = ["are", "is"];
- let random = ["the best human in the world", "the wors human in the world", "a drunk and serial killer", "a charity worker with rage drive problems"]
-
+let pronouns = "You";
+let verbs = "are";
+let random = ["the best human in the world", "the worst human in the world", "a drunk and serial killer", "a charity worker with rage drive problems", "akward to look at", "handomse"];
 // we start with the function
 
 /*To get a random element we need a random index. A random index is an integer number greater or equal to 0 and less than the number of elements.
@@ -19,20 +18,27 @@ const ramdomNumber = num => {
 By calling getMessage with the number of elements in the array (cards.length), we get a random index.*/
 
 // make a generic utility for retrieving a random element from any kind of array, including the array of cards.
+ 
 const getMessage = arr => {
     const ramdomIndex = ramdomNumber(arr.length);
-    return arr[ramdomIndex];
+    return arr[ramdomIndex]; // Index is going to get you one item out of the array. Your random number will make sure that it is a random item from the array that is selected.
 };
 
 //console.log(getMessage(pronouns));
 
 // now we store each ramdom message in variables
 
-let finalPart = getMessage(random);
+const result = () => {
+    let finalPart = getMessage(random);
+    return `${pronouns} ${verbs} ${finalPart}`
+};
 
-console.log (`${pronouns} ${verbs} ${finalPart}`);
+//console.log(result())
 
+const printMessage = () => {
+  document.querySelector(".text").style.display = "block";
+  document.querySelector(".text").innerHTML = result();
+};
 
-
-
-
+const generateButton = document.getElementById("button");
+  generateButton.addEventListener("click", printMessage);
